c047656df5da8bee7f7378789c732770b0e31014
Jaime Linares <32104719+parziva-1@users.noreply.github.com>
2020-02-01 10:57:17 -0500

    Create player.cs

---
+++ player.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/scripts/player.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Player : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Player()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""player"",
+    ""maps"": [
+        {
+            ""name"": ""player"",
+            ""id"": ""624938fb-756f-4f7e-a0da-6efb5b07810a"",
+            ""actions"": [
+                {
+                    ""name"": ""move"",
+                    ""type"": ""Button"",
+                    ""id"": ""841acc15-aabb-4ed3-951b-4102ffa8c00e"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""837dc74b-b388-4df6-963a-469c5d767360"",
+                    ""path"": ""<Joystick>/stick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""01e40bec-5252-4ffe-9294-6d462acdbd12"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""d783fcab-585b-4bc3-a805-aef381e0e373"",
+                    ""path"": ""<Touchscreen>/delta/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""20eba9b4-62ff-487e-b7ca-d21272cb712c"",
+                    ""path"": ""<Joystick>/stick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""a77a86f7-98b2-49fc-b699-a3d4932232d2"",
+                    ""path"": ""<Joystick>/stick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0b5e8b2f-201a-4d1c-99ad-fa8f397d0b4a"",
+                    ""path"": ""<Joystick>/stick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // player
+        m_player = asset.FindActionMap("player", throwIfNotFound: true);
+        m_player_move = m_player.FindAction("move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // player
+    private readonly InputActionMap m_player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_player_move;
+    public struct PlayerActions
+    {
+        private @Player m_Wrapper;
+        public PlayerActions(@Player wrapper) { m_Wrapper = wrapper; }
+        public InputAction @move => m_Wrapper.m_player_move;
+        public InputActionMap Get() { return m_Wrapper.m_player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @move.started += instance.OnMove;
+                @move.performed += instance.OnMove;
+                @move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerActions @player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
